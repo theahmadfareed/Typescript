74933618c16461deccf642574a0f51d4ff4a6827
theahmadfareed <ha5403905@gmail.com>
2024-02-09 00:10:35 +0500

    today work done.

---
+++ 0-Concepts/plist.ts
@@ -0,0 +1,53 @@
+console.log("Thapa List");
+
+/**
+ * !  
+ * ? 
+ * * 
+ * @param
+ * TODO
+ */
+
+//! Data Types (number, string, boolean, BigInt, any, unknown, )
+//! Type Inference (ability of the TSC to automatically define and assign types to variables, expressions and function return values based on their usage and context in the code.) 
+//! Type Annotation (explicitly defining the tyepe)  
+//! Functions (function_declaration, annonymous_function, arrow_functions)
+//! Optional and Default parameters   
+//! Strings
+/**
+ * * chartAt(), indexOf()
+ * * toLowerCase(), toUpperCase()
+ * * slice, split(), reverse(), join(), concat()
+ * * toString()
+ * * length()
+ *  */  
+//! Arrays ([1,2,3], new Array(1,2,3), Array.of(1,2,3))
+/**
+ * * push(), pop()
+ * * shift(), unshift()
+ * * sort()
+ * * forEach()
+ * * map(), filter(), reduce()
+ */
+//! Loops (for, for in, for of, forEach)  
+//! Objects ({name: "ali"})
+/**
+ * * assign()
+ * * create()
+ * * entries()
+ * * values()
+ * * is()
+ * * freeze()
+ * * valueOf(), toString(), toLocalString()
+ * * isPrototypeOf()
+ */
+//! Type Alisas, Enums, Tuples(readOnly)
+//! Unions(|) and Intersections(&)
+//! Generics (<T,U>)
+//!  
+//! 
+//?   
+//
+//*
+//TODO
+
