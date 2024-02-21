ec9c5a23ffe32844b8486f5b322da8fc7f52859c
theahmadfareed <ha5403905@gmail.com>
2024-02-22 00:02:46 +0500

    bug fixed.

--- 1-Web Dev/Frontend/React/todo-app/src/App.tsx
+++ 1-Web Dev/Frontend/React/todo-app/src/App.tsx
@@ -2,11 +2,13 @@ import AddToDo from "./components/addtodo"
 import Navbar from "./components/navbar"
 import Todos from "./components/todos"
 import "./App.css"
+import { RiTodoLine } from "react-icons/ri";
+
 
 const App = () => {
   return (
     <main>
-      <h1>TODO REACT + TYPESCRIPT </h1>
+      <h2><RiTodoLine className="icons" /> TODO REACT + TYPESCRIPT <RiTodoLine className="icons" /> </h2>
       <Navbar />
       <AddToDo />
       <Todos />
